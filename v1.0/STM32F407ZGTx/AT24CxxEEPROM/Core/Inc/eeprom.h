61f2f1ddc9b71ab4482a60f8b737f26e60f80144
Half road <git@halfroad.com>
2023-10-11 20:50:43 +0800

    Refactored the folders.

---
+++ v1.0/STM32F407ZGTx/AT24CxxEEPROM/Core/Inc/eeprom.h
@@ -0,0 +1,17 @@
+/*
+ * eeprom.h
+ *
+ *  Created on: Sep 10, 2023
+ *      Author: jinhui
+ */
+
+#ifndef INC_EEPROM_H_
+#define INC_EEPROM_H_
+
+#include "i2c.h"
+
+
+void write();
+void read();
+
+#endif /* INC_EEPROM_H_ */
---
+++ v1.0/STM32F407ZGTx/InterIntegratedCircuit/Core/Inc/due.h
@@ -0,0 +1,13 @@
+/*
+ * due.h
+ *
+ *  Created on: Aug 4, 2023
+ *      Author: Half Road
+ */
+
+#ifndef INC_DUE_H_
+#define INC_DUE_H_
+
+#include "i2c.h"
+
+#endif /* INC_DUE_H_ */
